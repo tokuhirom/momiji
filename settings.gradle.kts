168a9395efc2a5221c9bb6bf95814a3da0920a6d
Tokuhiro Matsuno <tokuhirom@gmail.com>
2024-07-22 12:40:28 +0900

    initial import

---
+++ settings.gradle.kts
@@ -0,0 +1,13 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
+}
+
+rootProject.name = "momiji"
+
